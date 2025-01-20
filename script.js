25c195d841d30b3a012ab0a9a116b3e174345a2a
elfespredator <135436015+elfespredator@users.noreply.github.com>
2025-01-20 21:05:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -45,12 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
             updateAnimation();
         });
     });
-<button id="testButton">Test</button>
-<script>
-    document.getElementById('testButton').addEventListener('click', function() {
-        alert('Bouton cliqué!');
-    });
-</script>
+
 
     // Initial animation update
     updateAnimation();
